8c7979b852b0873f4d30cc2fc4813c8ca8bc3f1a
Jose Salinas <rc.salinas.jose@leadps.org>
2016-12-06 17:17:26 +0000

    Challenge

---
+++ problem_sets/pumapix.py
@@ -0,0 +1,25 @@
+# two variables, one for the the while statement and the other for an empty list
+x = 0
+y = []
+
+while x != "done":
+	print("What are your favorite shows? Type in 'done' when you're finished") 
+	show = raw_input()
+	if show == "done":
+        	x = "done"
+	y.append(show) 
+y.remove("done")
+print("Your favorite shows are:" + str(y))
+
+
+print("Here's a few more:")
+# variable used to create a numerical list (not the coding type)
+num = 1
+y.append("The wire")
+y.append("Breaking Bad")
+y.sort()
+for favs in y:
+	print(str(num) + ". " + favs)
+	num = num + 1
+	
+
