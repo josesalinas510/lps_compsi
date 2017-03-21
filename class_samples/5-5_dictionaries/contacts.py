03b081eab6cd23c8357caff5ee379d450693bbf6
Jose Salinas <rc.salinas.jose@leadps.org>
2017-03-21 17:00:07 +0000

    extra credit added

--- class_samples/5-5_dictionaries/contacts.py
+++ class_samples/5-5_dictionaries/contacts.py
@@ -14,6 +14,8 @@ while codeRunning:
 	print("(1) Add a phone number")
 	print("(2) Print the full list of contacts")
 	print("(3) Enter a name to retrieve the contacts's phone number")
+	print("(4) Delete Contact")
+	print("(5) Change Contact number")
 	print("(0) Exit the Contact app")
 	input = raw_input()
 
@@ -29,6 +31,18 @@ while codeRunning:
 		print("What is the contact name under?")
 		Contact = raw_input()
 		print(myContacts[Contact])
+	elif input == "4":
+		print('What is the contact name under?')
+		name = raw_input()
+		del myContacts[name]
+	elif input == "5":
+		print("Which contact do you want to edit?")
+		contactName = raw_input()
+		print("What is their number?")
+		contactNumber = raw_input()
+		myContacts[contactName] = contactNumber
+		print(contactName + "'s number has been changed to " + myContacts[contactName])
 	elif input == "0":
+		print("Thank you")
 		codeRunning = False
 
